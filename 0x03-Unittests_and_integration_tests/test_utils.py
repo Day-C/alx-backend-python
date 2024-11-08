d43f694004378027e283e91bb010eb7588cd9156
Dayc <kongranalanyuy@gmail.com>
2024-11-08 21:31:57 +0000

    advanced testing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """testing utils functions."""
 from parameterized import parameterized
-from utils.py import access_nested_map
+from utils import access_nested_map
 import unittest
 from typing import (
         Mapping,
@@ -10,17 +10,25 @@ from typing import (
 )
 
 
-class TestAessNestedMap(unittest.TastCase):
+class TestAessNestedMap(unittest.TestCase):
     """Teatting utils functions and classes."""
 
     @parameterized.expand([
-        ({"a": 1}, ("a")),
-        ({"a": {"b": 2}}, ("a")),
-        ({"a": {"b": 2}}, ("a", "b"))
+        ({"a": 1}, ("a"), 1),
+        ({"a": {"b": 2}}, ("a"), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(nm: Mapping, pt: Sequence, result: Any) -> Any:
-        """test method for access_nested_map function."""
-        self.assertEqual(access_nested_map(nm, pt), result)
+    def test_access_nested_map(self, nested_map, path, expected_result) -> Any:
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected_result)
+
+    @parameterized.expand([
+        ({}, ("a")),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
 
 
 if __name__ == "__main__":
