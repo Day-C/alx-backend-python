536149f82a3350a47c71389c59ea315dcf9af8ef
DayC <Kongranalanyuy@gmail.com>
2025-01-03 13:50:38 -0800

    Callable()

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/urs/bin/env python3
+'''Return a function that multiplies a float.'''
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ return another function
+    Args: multipier (float)
+    Return: a fucntion that multiplies a float by multiplier.
+    """
+
+    return lambda x: x * multiplier
