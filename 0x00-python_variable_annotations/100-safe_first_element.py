27095126857c29b7d5b1ad44393e095d565a73ac
DayC <Kongranalanyuy@gmail.com>
2025-01-03 14:09:06 -0800

    Duck typing

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,6 +2,7 @@
 '''annotate.'''
 from typing import Union, Sequence, Any
 
+
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """annotation function to save first element of any type
     Args:
