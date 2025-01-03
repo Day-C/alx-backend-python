c08e700782779ab83e9321331d3e6f2f4690f77b
DayC <Kongranalanyuy@gmail.com>
2025-01-03 13:34:16 -0800

    convert arg to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Type annoted function to return string."""
+
+
+def to_str(n: float) -> str:
+    """return the string representation of func argument.
+    Args:
+      n: float
+    return : string representation of n.
+    """
+
+    return str(n)
