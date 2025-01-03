ee7f118f369d2380145c99c6f4172f253df89492
DayC <Kongranalanyuy@gmail.com>
2025-01-03 13:31:46 -0800

    float abs

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Anotated function to return floor of a float."""
+
+
+def floor(n: float) -> int:
+    '''get the floor of a float number
+    example: 23.25 ..--> 23.
+    '''
+
+    return int(n)
