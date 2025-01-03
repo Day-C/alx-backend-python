55cba36abace721e2165e62c89e77b3b790ac30c
DayC <Kongranalanyuy@gmail.com>
2025-01-03 13:28:50 -0800

    string concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Write a type anotation function name concat.'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Contat 2 string
+    Args:
+      str1: (string)
+      str2: (string)
+    return concatenated strings.
+    '''
+
+    return f'{str1}{str2}'
