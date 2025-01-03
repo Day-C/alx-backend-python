7f3f4e41da38e7c9f27bd5ad0b28f1fe728f3fc0
DayC <Kongranalanyuy@gmail.com>
2025-01-03 14:06:00 -0800

    duck type and iterable objs

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,6 @@ from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    '''get a list of tuple integers.'''
+
     return [(i, len(i)) for i in lst]
