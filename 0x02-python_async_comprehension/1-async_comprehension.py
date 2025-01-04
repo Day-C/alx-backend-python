a2dd2a24ae0e9511b5898a7f54aaa451b7713768
DayC <kongranalanyuy@gmail.com>
2025-01-04 17:06:37 +0000

    write a coroutine

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list:
+async def async_comprehension() -> List[float]:
     """collect generators results using an async comprehensing."""
 
     all_vals = []
